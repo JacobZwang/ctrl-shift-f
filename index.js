6d4a6154057e1f5ed2d85c82a0c83ade3c35a04a
JacobZwang <59858341+JacobZwang@users.noreply.github.com>
2022-06-04 15:24:19 -0700

    render basic results for current page

--- index.js
+++ index.js
@@ -1,53 +1,11 @@
 (async function () {
-	const shadowElement = document.createElement("div");
-	document.body.appendChild(shadowElement);
-	let shadow = shadowElement.attachShadow({ mode: "open" });
-
-	const containerElement = document.createElement("div");
-	shadow.appendChild(containerElement);
-	Object.assign(containerElement.style, {
-		position: "fixed",
-		width: "20rem",
-		backgroundColor: "white",
-		left: "50vw",
-		transform: "translateX(-50%)",
-		top: "0",
-		zIndex: "10000",
-		padding: "0.5rem",
-		boxSizing: "border-box"
-	});
-
-	const inputElement = document.createElement("input");
-	containerElement.appendChild(inputElement);
-	Object.assign(inputElement.style, {
-		width: "100%",
-		boxSizing: "border-box"
-	});
-
-	inputElement.addEventListener("input", (e) => {
-		console.log(e.target.value);
-	});
-
-	const pageSearchParams = Object.fromEntries(
-		new URLSearchParams(document.location.search).entries()
-	);
-	const pagePath = document.location.pathname;
-	const pageText = document.body.innerText;
-
-	let search = "jacob";
-	const matches = Array.from(pageText.matchAll(new RegExp(search, "g"))).forEach((match) => {
-		console.log(match);
-	});
-
-	const storage = chrome.storage.sync;
-
 	// retrieve all cache from storage
 	const cache = await new Promise((resolve, reject) => {
 		/* Cache from all pages is compressed together for smallest total size.
         It is equally split across all keys to avoid max key size limitation. */
 
-		storage.get(null, (items) => {
-			// this is how documentation says to do detect errors.
+		chrome.storage.sync.get(null, (items) => {
+			// this is how documentation says to detect errors
 			// https://developer.chrome.com/docs/extensions/reference/storage/#asynchronous-preload-from-storage
 			if (chrome.runtime.lastError) {
 				throw new Error("could not cache page", {
@@ -57,6 +15,65 @@
 
 			resolve(Object.values(items).join(""));
 		});
-	});
-	// .then((data) => JSON.parse(data)); // TODO: un-compress data
+	}).then((data) => JSON.parse(data || "{}")); // TODO: un-compress data
+
+	const pageSearchParams = Object.fromEntries(
+		new URLSearchParams(document.location.search).entries()
+	);
+	const pagePath = document.location.pathname;
+	const pageText = document.body.innerText.replace(/\s+/g, " ").trim();
+
+	// ui for search input and results
+	(function () {
+		const shadowElement = document.createElement("div");
+		document.body.appendChild(shadowElement);
+		let shadow = shadowElement.attachShadow({ mode: "open" });
+
+		const containerElement = document.createElement("div");
+		shadow.appendChild(containerElement);
+		Object.assign(containerElement.style, {
+			position: "fixed",
+			width: "20rem",
+			backgroundColor: "white",
+			left: "50vw",
+			transform: "translateX(-50%)",
+			top: "0",
+			zIndex: "10000",
+			padding: "0.5rem",
+			boxSizing: "border-box"
+		});
+
+		const inputElement = document.createElement("input");
+		containerElement.appendChild(inputElement);
+		Object.assign(inputElement.style, {
+			width: "100%",
+			boxSizing: "border-box"
+		});
+
+		const resultsElement = document.createElement("div");
+		Object.assign(resultsElement.style, {
+			maxHeight: "8rem",
+			overflowY: "scroll"
+		});
+		containerElement.appendChild(resultsElement);
+
+		inputElement.addEventListener("input", (e) => {
+			if (e.target.value.length < 1) return;
+
+			resultsElement.innerHTML = "";
+			const matches = Array.from(pageText.matchAll(new RegExp(e.target.value, "g"))).forEach(
+				(match) => {
+					const link = document.createElement("a");
+					resultsElement.appendChild(link);
+					link.innerText = pageText.substring(match.index - 25, match.index + 25);
+					Object.assign(link.style, {
+						display: "block",
+						width: "100%",
+						padding: "0.25rem",
+						borderBottom: "1px solid grey"
+					});
+				}
+			);
+		});
+	})();
 })();
