8d8529fbeeaa0222043ff9459ef554e8a65fb62f
JacobZwang <59858341+JacobZwang@users.noreply.github.com>
2022-06-04 13:01:35 -0700

    start on storage model

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
-const pageText = document.body.textContent;
-console.log(pageText);
+(async function () {
+	const search = Object.fromEntries(new URLSearchParams(document.location.search).entries());
+	const path = document.location.pathname;
+	const text = document.body.innerText;
+
+	const storage = chrome.storage.sync;
+
+	// retrieve all cache from storage
+	const cache = await new Promise((resolve, resolve) => {
+		/* Cache from all pages is compressed together for smallest total size.
+        It is equally split across all keys to avoid max key size limitation. */
+
+		storage.get(null, (items) => {
+			// this is how documentation says to do detect errors.
+			// https://developer.chrome.com/docs/extensions/reference/storage/#asynchronous-preload-from-storage
+			if (chrome.runtime.lastError) {
+				throw new Error("could not cache page", {
+					cause: chrome.runtime.lastError
+				});
+			}
+
+			resolve(items.values.join(""));
+		});
+	}).then((data) => JSON.parse(data)); // TODO: un-compress data
+})();
