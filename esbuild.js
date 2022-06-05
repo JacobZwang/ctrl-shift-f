36be2ecf2f3afb4ba7f0aacb74955e90dc2d0a14
JacobZwang <59858341+JacobZwang@users.noreply.github.com>
2022-06-05 14:30:08 -0700

    bundle for future

--- esbuild.js
+++ esbuild.js
@@ -1,8 +1,19 @@
 import esbuild from "esbuild";
+import fs from "fs";
 
-esbuild.build({
-	entryPoints: ["index.jsx"],
-	outfile: "index.js",
-	bundle: true,
-	minify: true
-});
+esbuild
+	.build({
+		entryPoints: ["index.jsx"],
+		outfile: "index.js",
+		bundle: true,
+		minify: true
+	})
+	.then(() => {
+		fs.rmSync("./bundle", {
+			recursive: true
+		});
+		fs.mkdirSync("./bundle", {});
+		fs.copyFileSync("./index.js", "./bundle/index.js");
+		fs.copyFileSync("./popup.html", "./bundle/popup.js");
+		fs.copyFileSync("./manifest.json", "./bundle/manifest.json");
+	});
